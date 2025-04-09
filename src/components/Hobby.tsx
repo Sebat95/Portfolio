2ce57f0d6960e9a31600918e96e390b1915c5286
Sebat95 <samubattaglino@gmail.com>
2025-04-09 15:12:07 +0200

    Update Hobby.tsx

--- src/components/Hobby.tsx
+++ src/components/Hobby.tsx
@@ -1,26 +1,25 @@
-import { Dispatch } from "react";
-import { RefProps } from "../common/RefProps";
+import { useState } from "react";
+import { HobbyObjectProps, RefProps } from "../common/props";
 import React from "react";
 
 interface HobbyProps extends RefProps {
-    ind: number;
-    setHighlighted: Dispatch<React.SetStateAction<number>>;
+  highlighted: boolean
 }
 
 const Hobby = (props: HobbyProps) => {
-    const childProps = {...props} as RefProps;
+    const [hover, setHover] = useState(false);
+    const childProps = {...props} as HobbyObjectProps;
     if(childProps.position){
         delete childProps.position;
     };
+    childProps.emissive = hover;
 
     return (
         <group {...props} dispose={null} position={props.position} ref={props.innerRef}
-            onClick={() => props.setHighlighted((prev) => prev == -1 ? props.ind : -1)}>
-            { React.cloneElement(props.children as JSX.Element, {...childProps})}
-            <mesh scale={2}>
-                <sphereGeometry />
-                <meshStandardMaterial opacity={0.5} color={'white'} emissive={'white'} emissiveIntensity={2} transparent />
-            </mesh>
+            onPointerEnter={() => setHover(!props.highlighted)} onPointerLeave={() => setHover(false)}>
+                {
+                    React.cloneElement(React.Children.only(props.children) as JSX.Element, {...childProps})
+                }
         </group>
     )
 }
