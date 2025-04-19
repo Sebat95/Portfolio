4770291809740c33bfb18866cfcdabfa3e51bf8b
Sebat95 <samubattaglino@gmail.com>
2025-04-19 21:21:20 +0200

    change navbar text

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = (props: NavProps) => {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a href="/" className="text-neutral-400 font-semibold text-xl hover:text-white transition-colors">
-            Welcome to my personal site
+            Samuele Battaglino
           </a>
           {/* Smaller device navbar collapsing button */}
           <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none
